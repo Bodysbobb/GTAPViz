6e99d6394350b25edbe73d337e1e1138af85f07a
Bodysbobb <97965877+Bodysbobb@users.noreply.github.com>
2025-02-13 04:49:08 -0500

    Initial commit

---
+++ GEMPACKviaR.R
@@ -0,0 +1,38 @@
+# ==============================================================================
+# PROJECT: Importing GTAP Results from .SL4 and .HAR to CSV/STATA/R/TXT Using R  
+# ==============================================================================
+# DATE: February 2025  
+# VERSION: 1.0  
+#
+# AUTHORS:  
+# (1) Pattawee Puangchit, Ph.D. Candidate in Agricultural Economics  
+#     Purdue University & Research Assistant at GTAP  
+#     (Contact: ppuangch@purdue.edu)  
+#
+# (2) Dr. Erwin Corong, Principal Research Economist and Associate Director  
+#
+# ==============================================================================
+# ACKNOWLEDGMENTS:  
+# This project utilizes the R package developed by Maros Ivanic (2023)  
+# for processing GTAP-related data.  
+#
+# We acknowledge the contributions of the developers in making GTAP  
+# data handling more accessible in R.
+#
+# ==============================================================================
+# DESCRIPTION:  
+# This script facilitates the conversion of GTAP results from .SL4 and .HAR formats  
+# into CSV, STATA, R, and TXT formats for further analysis.  
+#
+# A fully detailed manuscript, including an R Notebook version of this script,  
+# is available at: <waiting for publication link>.  
+#
+# A Python implementation of this script is also available. For more details, please see:  
+# <waiting for publication link>.  
+#
+# ==============================================================================
+# IMPORTANT:  
+# The GTAP Model and database require a separate license, which can be obtained at:  
+# https://www.gtap.agecon.purdue.edu/databases/pricing.asp  
+# ==============================================================================
+
